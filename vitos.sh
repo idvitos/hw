63d5fe6e93cc201588633b31f33a72b6e85473ea
idvitos <v.popovych@internetdevels.com>
2016-11-01 15:31:07 +0200

    bash arguments

---
+++ vitos.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## Arguments reference:
+# $# --> number of arguments that our script has
+# $0 --> the file name of our script
+# $1...$n --> script arguments
+
+# ourfilename=$0
+# echo $ourfilename
+
+# num_arguments=$#
+
+me="Я бог Біхата"
+tsar="Царь-батюшка"
+if [: {1},: {2},: {3} ]; then
+	echo "$me, ${1} ${2} ${3}"
+else 
+	echo "$tsar"
+fi
